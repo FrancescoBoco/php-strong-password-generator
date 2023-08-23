e6433b34c131fdb7329ae85b7c868d8f69b174db
FrancescoBoco <francesco.bocoux@gmail.com>
2023-08-23 16:27:57 +0200

    milestone 2

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
-// GENERATORE RANDOM DI TRINGHE 
-function generateRandomString($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[random_int(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-if(isset($_GET['password-length'])){
-echo generateRandomString($_GET['password-length']);
-}
-
-
-
-if( isset($_GET['password-length']) ){
-    $input = $_GET['password-length'];
-// var_dump($_GET['password-length']);
-
-}
-// if(isset($_GET['password-length']) != )
-
+include __DIR__ .'functions.php';
 ?>
 
 <!DOCTYPE html>
